e5916f566233ab00b9ecb164c92cce78cf343c39
hissoune <khalidhissoune962@gmail.com>
2024-11-26 15:31:17 +0100

    Add : adding the guard and check on the user

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,37 @@
+import { ExecutionContext, UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { User } from "src/users/entities/user.entity";
+
+
+
+export  class AuthGuard {
+    
+    constructor(@InjectModel(User.name) private readonly userModel: Model<User>,
+               private readonly jwtService: JwtService
+            ) {}
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest();
+
+        const token = request.headers.authorization?.split(' ')[1];
+
+        if (!token) {
+          throw new UnauthorizedException('No token provided');
+        }
+        try {
+            const decoded = this.jwtService.verify(token);
+
+            const user = await this.userModel.findOne({email:decoded.email}); 
+
+            request.user = user;
+      
+            return true
+
+        } catch (error) {
+             
+        }
+    
+    }
+}

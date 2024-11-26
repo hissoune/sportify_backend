7fd829079f850b2315cb803be9c0ad49234dbc6f
hissoune <khalidhissoune962@gmail.com>
2024-11-26 15:36:01 +0100

    Clear : clearing the module of the user from the unnecessary modules

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,22 +3,16 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, userSchema } from './entities/user.entity';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Module({
   imports:[
     MongooseModule.forFeature([{name:User.name,schema:userSchema}]),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'), 
-        signOptions: { expiresIn: '1h' },
-      })
-    }),
+    
   ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService,AuthGuard],
+  exports:[AuthGuard]
 })
 export class UsersModule {}

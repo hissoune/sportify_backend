3076da58b10c07fb26381e6e2742725a0497e441
hissoune <khalidhissoune962@gmail.com>
2024-11-26 13:23:27 +0100

    Add : adding the login func in the service and return the token

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { hashPassword } from 'src/utils/password.util';
+import { comparePassword, hashPassword } from 'src/utils/password.util';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/user.entity';
 import { Model } from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UsersService {
- constructor(@InjectModel(User.name) private userModel: Model<User>){}
+ constructor(@InjectModel(User.name) private userModel: Model<User>,
+            private readonly jwtService: JwtService
+){}
 
  async create(createUserDto: CreateUserDto) {
     const hashedPassword = await hashPassword(createUserDto.password);
@@ -18,6 +21,28 @@ export class UsersService {
   
     
     return newUser.save();
+  }
+  async login (createUserDto: CreateUserDto):Promise<{token:string}>{
+
+    const existUser = await this.userModel.findOne({email:createUserDto.email}).exec();
+
+    if (!existUser) {
+
+      throw new UnauthorizedException('Invalid email or password');
+    }
+    const isPasswordValid = await comparePassword(createUserDto.password, existUser.password);
+
+    if (!isPasswordValid) {
+
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const token = this.jwtService.sign({  name: existUser.name, email: existUser.email });
+
+   
+    return { token };
+    
+
   }
 
   findAll() {

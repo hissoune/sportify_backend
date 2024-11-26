2a78c0118338527aa63285c74de2ab2ff60cb5c3
hissoune <khalidhissoune962@gmail.com>
2024-11-26 15:33:35 +0100

    Add : adding the auth to the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
@@ -10,14 +11,16 @@ import { MongooseModule } from '@nestjs/mongoose';
       isGlobal: true,
     }),
     MongooseModule.forRootAsync({
-      
+
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         uri: configService.get<string>('MONGO_URI'),
       }),
       inject: [ConfigService],
     }),
-    UsersModule],
+    UsersModule,
+    AuthModule
+  ],
   controllers: [],
   providers: [],
 })

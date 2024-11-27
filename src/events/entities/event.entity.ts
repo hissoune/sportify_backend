2ab96369011bbef92c73b86d798c39c7356ba978
hissoune <khalidhissoune962@gmail.com>
2024-11-27 09:37:38 +0100

    Add : adding the event schema

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,1 +1,26 @@
-export class Event {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Types } from "mongoose";
+
+
+@Schema({timestamps:true})
+
+export class Event extends Document {
+
+    @Prop({ required: true })
+    name: string;
+  
+    @Prop({ required: true })
+    date: Date;
+  
+    @Prop({ required: true })
+    location: string;
+
+    @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }] })
+    participants: Types.ObjectId[];
+
+    @Prop({ type: Types.ObjectId, ref: 'User', required: true }) 
+     owner: Types.ObjectId;
+
+ 
+}
+export const eventSchema = SchemaFactory.createForClass(Event)

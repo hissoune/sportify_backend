08d8cd41b102ecf10916651e94f2159f97c8290f
hissoune <khalidhissoune962@gmail.com>
2024-11-27 17:15:21 +0100

    Fix : fixing the problem of the midlware cant get the secret key of the event module  module

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, userSchema } from 'src/users/entities/user.entity';
+import { Event, eventSchema } from './entities/event.entity';
+
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{name:User.name,schema:userSchema},{name:Event.name,schema:eventSchema}]),
+  
+  ],
   controllers: [EventsController],
   providers: [EventsService],
 })

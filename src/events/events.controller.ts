1fba673358687552804997b91dfb568f16516f8d
hissoune <khalidhissoune962@gmail.com>
2024-11-27 17:16:34 +0100

    Add : adding the expectation of the image in the controller of the event

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,14 +1,69 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  UseInterceptors,
+  UploadedFile,
+  Req,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid'; // To generate unique filenames
+import * as path from 'path';
 
 @Controller('events')
+@UseGuards(AuthGuard)
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
   @Post()
-  createEvent(@Body() createEventDto: CreateEventDto) {
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './uploads', 
+        filename: (req, file, callback) => {
+          const uniqueSuffix = `${uuidv4()}${path.extname(file.originalname)}`;
+          callback(null, uniqueSuffix); 
+        },
+      }),
+      fileFilter: (req, file, callback) => {
+        const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+        if (allowedMimeTypes.includes(file.mimetype)) {
+          console.log(allowedMimeTypes);
+          
+          callback(null, true);
+        } else {
+          callback(new Error('Invalid file type. Only JPEG, PNG, and GIF are allowed.'), false);
+        }
+      },
+    }),
+  )
+  createEvent(
+    @Body() createEventDto: CreateEventDto,
+    @Req() req,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    if (!file) {
+      throw new UnauthorizedException('Image is required');
+    }
+
+    const owner = req.user.id; 
+    createEventDto.owner = owner;
+    console.log(createEventDto);
+    
+    createEventDto.imagePath = file.filename; 
+      console.log(createEventDto);
+      
     return this.eventsService.createEvent(createEventDto);
   }
 
@@ -16,6 +71,8 @@ export class EventsController {
   getAllEvents() {
     return this.eventsService.getAllEvents();
   }
+
+  
 
   @Get(':id')
   getEventById(@Param('id') id: string) {

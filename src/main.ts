93b605951ffcafb5bd3640ee62fb6fcdc7c7cadb
hissoune <khalidhissoune962@gmail.com>
2024-11-29 05:45:01 +0100

    Fix : fixing the cors to accept the patch

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.use(
     cors({
       origin: 'http://localhost:5173',
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
       credentials: true,
     }),
   );

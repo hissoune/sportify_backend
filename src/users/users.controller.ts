e02539138adffb8282384b495e63f199b2f283e3
hissoune <khalidhissoune962@gmail.com>
2024-12-06 10:01:49 +0100

    Fix: Corrected route handling in users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,51 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, UseInterceptors, UploadedFile, UnauthorizedException, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
-import { log } from 'console';
-
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import * as path from 'path';
+import { v4 as uuidv4 } from 'uuid';
 @Controller('users')
 @UseGuards(AuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  createUser(@Body() createUserDto: CreateUserDto) {
-  
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          const uniqueSuffix = `${uuidv4()}${path.extname(file.originalname)}`;
+          callback(null, uniqueSuffix);
+        },
+      }),
+      fileFilter: (req, file, callback) => {
+        const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+        if (allowedMimeTypes.includes(file.mimetype)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Invalid file type. Only JPEG, PNG, and GIF are allowed.'), false);
+        }
+      },
+    }),
+  )
+  createUser(
     
+    @Body() createUserDto: CreateUserDto,
+    @Req() req,
+    @UploadedFile() file: Express.Multer.File,
+  
+  ) {
+    if (!file) {
+      throw new UnauthorizedException('Image is required');
+    }
+    
+    const serverUrl = `${req.protocol}://${req.get('host')}`; 
+    createUserDto.imagePath = `${serverUrl}/uploads/${file.filename}`;
+
       return this.usersService.createUser(createUserDto);
   }
  
